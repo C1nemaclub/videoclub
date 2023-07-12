78e7c4ee71078b2acf79438597ad4dbe22a419aa
C1nemaclub <santiagov801@gmail.com>
2023-07-11 19:38:07 -0500

    server call-answer architecure

--- server/index.js
+++ server/index.js
@@ -22,14 +22,28 @@ const io = new Server(server, {
 const users = {};
 
 io.on('connection', (socket) => {
-
   console.log(`User ${socket.id} connected`);
   users[socket.id] = {
     id: socket.id,
   };
 
+  const otherUsers = getOthersUsers(socket.id);
+  socket.emit('all-users', otherUsers);
+
+  socket.on('call-user', (payload)=>{
+    io.to(payload.userToCall).emit('user-joined', {signal: payload.signal, callerID: payload.callerID})
+  })
+
+  socket.on('answer-call', (payload)=>{
+    io.to(payload.callerID).emit('call-accepted', {signal: payload.signal, id: socket.id})
+  })
+
   socket.on('disconnect', () => {
+    delete users[socket.id]
     console.log(`User ${socket.id} disconnected`);
   });
-  
 });
+
+const getOthersUsers = (myID) => {
+  return Object.values(users).filter((user) => user.id !== myID);
+};
